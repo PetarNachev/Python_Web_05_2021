d2e1e88c57691690bc0d9cabe5704dcd0b85b0a7
PetarNachev <p.nachev92@gmail.com>
2021-08-05 22:39:20 +0300

    Start
    Starting the thing

---
+++ bosozoku/bosozoku/accounts/models.py
@@ -0,0 +1,37 @@
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.db import models
+
+from bosozoku.accounts.managers import BosozokuUserManager
+
+
+class BosozokuUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(
+        unique=True,
+    )
+
+    is_staff = models.BooleanField(
+        default=False,
+    )
+    USERNAME_FIELD = 'email'
+
+    date_joined = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    objects = BosozokuUserManager()
+
+
+class Profile(models.Model):
+    profile_image = models.ImageField(
+        upload_to='profiles',
+        blank=True,
+    )
+    user = models.OneToOneField(
+        BosozokuUser,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+
+
+from .signals import *
---
+++ bosozoku/bosozoku/accounts/views.py
@@ -0,0 +1,45 @@
+from django.contrib.auth import logout, login
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+
+from bosozoku.accounts.forms import LoginForm, RegisterForm, ProfileForm
+
+
+def login_user(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+    else:
+        form = LoginForm()
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'accounts/login.html', context)
+
+
+def register_user(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+    else:
+        form = RegisterForm()
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'accounts/register.html', context)
+
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('index')
