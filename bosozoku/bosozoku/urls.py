ff6195b012a88a0df95f291741afd2321bf66912
PetarNachev <p.nachev92@gmail.com>
2021-08-05 23:06:31 +0300

    template updates
    working on base template

--- bosozoku/bosozoku/urls.py
+++ bosozoku/bosozoku/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('bosozoku.common.urls')),
+    path('accounts/', include('bosozoku.accounts.urls')),
 ]
